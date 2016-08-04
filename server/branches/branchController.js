e752242e3280e1ecaad5ebb46c4a245a8d574bbb
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-05 01:17:05 +0300

    Create BranchModel and BranchController

---
+++ server/branches/branchController.js
@@ -0,0 +1,16 @@
+var Branch = require('./branchModel.js');
+
+module.exports = {
+
+  getAllBranches : function (req , res) {
+    Branch.find().exec(function (error , branches) {
+      if(error)
+        res.status(500).send(error);
+      else
+        res.status(200).send(branches);
+    })
+  },
+
+
+
+};
