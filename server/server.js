55d7c7dce5b88bc5eb4224778310f19db3dc7144
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-04 23:17:34 +0300

    Create server file

---
+++ server/server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+var server = require ('http').createServer(app);
+
+var mongoURI = 'mongodb://localhost/survey-GS';
+mongoose.connect(mongoURI);
+db = mongoose.connection;
+
+var port = 3000;
+db.once('open',function () {
+	console.log('mongoDB is open');
+});
+
+require('./config/middleware.js') (app,express);
+require('./config/routes.js') (app,express);
+
+app.listen(port , function () {
+	console.log('Server now listening on port ' + port);
+});
+
+module.exports = app;
