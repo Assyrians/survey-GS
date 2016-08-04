84202727369c22d8104d2b68b2d0b77197bb6acb
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-05 02:56:49 +0300

    Implement questionModel

---
+++ server/questions/questionModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema;
+
+var questionSchema = new schema({
+	text : String 
+});
+
+var Question =mongoose.model('Question', questionSchema);
+
+// Test Question
+// var testQuestion =new Question({
+// 	text : "it test Question number 2"
+// });
+
+// testQuestion.save(function (err , newQuestion) {
+// 	console.log(newQuestion);
+// })
+
+module.exports = Question;
