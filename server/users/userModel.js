77eee8bec5e9f85ef5be7ef1b1ed9119acb483fc
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-05 00:22:01 +0300

    create users model closes #7

---
+++ server/users/userModel.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+
+var Schema = mongoose.Schema ;
+
+// This is the User Table .
+var userSchema = new Schema({
+	username : { type : String , required: true },
+	password : {type : String , required: true },
+	email  : {type:  String }
+});
+
+
+
+
+
+// Setting up the user Model . 
+var User = mongoose.model('User' , userSchema);
+
+// test user
+// var newUser=new User({
+// 	username : "admin",
+// 	password : "admin",
+// 	email : "admin@gmail.com"
+// });
+
+// newUser.save(function (err,newUser) {
+// 	console.log(newUser);
+// })
+
+module.exports = User;
+
+
+
+
+
+
+
+
