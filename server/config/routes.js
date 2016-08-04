3f2ef74ee2bd57c7a472d04781809f460a1cabc6
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-05 02:58:03 +0300

    Implement questionController

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,7 @@
 var helpers = require('./helpers.js');
 var userController = require('../users/userController.js');
+var branchController = require('../branches/branchController.js');
+var questionController = require('../questions/questionController.js');
 
 // exporting DB controller's functions
 module.exports = function(app, express){
@@ -7,6 +9,15 @@ module.exports = function(app, express){
 	app.post('/api/users/signin', userController.signin);
 	app.get('/api/users/signedin', userController.checkAuth);
 	app.get('/api/users/user/:id', userController.getOne);
+
+
+	app.get('/api/branches' ,branchController.getAllBranches ) ;
+	app.get('/api/branches/branch/:id' , branchController.getOneBranch ) ;
+
+
+	app.get('/api/questions' , questionController.getAllQuestion);
+	app.get('/api/questions/question/:id' , questionController.getOneQuestion);
+	app.post('/api/questions' , questionController.getSetOfQuestion);
 
 	// If a request is sent somewhere other than the routes above,
 	// send it through custom error handler
---
+++ server/questions/questionController.js
@@ -0,0 +1,35 @@
+var Question = require('../questions/questionModel.js');
+
+module.exports = {
+	getAllQuestion : function ( req , res) {
+		Question.find().exec(function (err , questions) {
+			if(err)
+				res.status(500).send(err);
+			else
+				res.status(200).send(questions);
+		})
+	},
+
+	getOneQuestion : function (req , res) {
+		Question.findOne({_id : req.params.id}).exec(function (err , question) {
+			if(err)
+				res.status(500).send(err);
+			else
+				res.status(200).send(question);
+		})
+	},
+
+
+	getSetOfQuestion : function (req , res) {
+		var arrayOfObjectIds=req.body.arrayOfObjectIds;
+		var arrayOfQuestionObject=[];
+		for (var i = 0; i < arrayOfObjectIds.length; i++) {
+			Question.findOne({_id:arrayOfObjectIds[i]}).exec(function (err , Question) {
+				arrayOfQuestionObject.push(Question);
+				if(arrayOfQuestionObject.length===arrayOfObjectIds.length){
+					res.status(200).send(arrayOfQuestionObject);
+				}
+			})
+		}
+	}
+}
