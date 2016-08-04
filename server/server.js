963831e7e7e664cc2e6811e8c10f1faded3acb08
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-05 00:20:17 +0300

    Edit server files

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
-
 var express = require('express');
 var mongoose = require('mongoose');
 
 var app = express();
+var server   = require('http').createServer(app);
 
 
-var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/extrus';
+var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/surveyGS';
 
 var port = process.env.PORT || 8000;
-// connect to mongo database named "extrus"
+// connect to mongo database named "surveyGS"
 mongoose.connect(mongoURI);
 
 // configure our server with all the middleware and routing
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
-
 // start listening to requests on port 8000
-app.listen(port, function(){
+server.listen(port, function(){
 	console.log('Server now listening on port ',port )
 });
 
