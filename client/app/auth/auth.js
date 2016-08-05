00426d581b1e50b2bc8252ba9d378daa0ab0d815
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-05 15:31:43 +0300

    created temporary signin function

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -2,4 +2,11 @@ angular.module('GreenSaloon.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location) {
 
+	$scope.user = {};
+
+	$scope.signIn = function(){
+		console.log($scope.user.username);
+		console.log($scope.user.password);
+	};
+	
 });
