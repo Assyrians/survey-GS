2d671b60fa15368fd2dcb6d403f126061064a0f6
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-05 03:08:45 +0300

    Implement formModel closes #4

---
+++ server/forms/formModel.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var schema =mongoose.Schema;
+
+var formSchema = new schema({
+	type : String ,
+	questions : [{ type: schema.Types.ObjectId , ref : 'Question'}]
+});
+
+var Form = mongoose.model('Form' , formSchema);
+
+
+// Form test
+// var newForm = new Form({
+// 	type : "Day",
+// 	questions : ['57a3d0b31e9bc0b8134b0215','57a3d0514fd172741bc19b68']
+// })
+
+// newForm.save(function (err , form) {
+// 	console.log(form);
+// })
+
+module.exports = Form;
