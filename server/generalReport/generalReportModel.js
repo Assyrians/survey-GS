bb31b24b1a69e503d0f4379de1d2e96c13a03bef
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-12 20:11:28 +0300

    Implement generalReportModel

---
+++ server/generalReport/generalReportModel.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema;
+
+var generalReportSchema = new schema ({
+	questions : [{_id : String , text : String , numOfYes : Number , numOfNo : Number, questionNum : Number}],
+	avgMark : Number,
+	bestMark : Number,
+	worstMark : Number,
+	monthlyVisits : Number
+});
+
+var GeneralReport = mongoose.model('GeneralReport', generalReportSchema);
+
+module.exports = GeneralReport;
+
+
