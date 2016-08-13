81a4a15c875a3573d01a86d1ea5985ba97aa7ee5
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-13 14:49:33 +0300

    Add alert to send email

--- client/app/reports/singleReportView.js
+++ client/app/reports/singleReportView.js
@@ -80,6 +80,7 @@ angular.module('GreenSaloon.singleReportView', [])
  		})
  		.then(function(resp){
  			console.log(resp);
+ 			arert("تم ارسال التقرير")
  		})
  		.catch(function(error){
  			console.log(error);
