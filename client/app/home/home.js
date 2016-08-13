642e316f73448cfe19f235866d628e13136046af
Bader Khalifeh <bader.a.khalifeh@gmail.com>
2016-08-13 14:19:22 +0300

    implemented delete user functionality

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -3,6 +3,19 @@ angular.module('GreenSaloon.home',[])
 .controller('HomeController', function($scope, $window, $mdDialog, $mdMedia, Auth, User, Dialogs){
   $scope.username = $window.localStorage.getItem('com.GS');
   
+  $scope.data = {};
+
+  $scope.initialize = function(){
+  	User.getAll()
+  	.then(function(resp){
+  		$scope.data.users = resp.data;
+  	})
+  	.catch(function(error){
+  		console.log(error);
+  	});
+  };
+
+  $scope.initialize();
 
   $scope.signout = function(){
     Auth.signout();
@@ -12,25 +25,43 @@ angular.module('GreenSaloon.home',[])
   	$mdDialog.hide(value);
   };
 
+  $scope.cancel = function(){
+  	$mdDialog.cancel();
+  }
+
   $scope.addUser = function(ev){
-  	Dialogs.showDialog($scope,$mdDialog,$mdMedia,
-		    'HomeController','app/home/addUser.html',ev,
-		    {},function(answer){
-		      if(answer){
-		      	User.addOne(answer)
-		      	.then(function(resp){
-		      		alert('تمت اضافة المستخدم بنجاح');
-		      	})
-		      	.catch(function(error){
-		      		alert('حدث خطأ اثناء الاضافة, تأكد ان اسم المستخدم غير موجود');
-		      	});
-		      }
-		    },function(){
-		      console.log('You cancelled the dialog.');
-		    });
+		Dialogs.showDialog($scope,$mdDialog,$mdMedia,
+	    'HomeController','app/home/addUser.html',ev,
+	    {},function(answer){
+	      if(answer){
+	      	User.addOne(answer)
+	      	.then(function(resp){
+	      		alert('تم اضافة المستخدم بنجاح');
+	      	})
+	      	.catch(function(error){
+	      		alert('حدث خطأ اثناء الاضافة, تأكد ان اسم المستخدم غير موجود');
+	      	});
+	      }
+	    },function(){
+	    });
   };
 
-  $scope.deleteUser = function(){
-
+  $scope.deleteUser = function(ev){
+		Dialogs.showDialog($scope,$mdDialog,$mdMedia,
+	    'HomeController','app/home/deleteUser.html',ev,
+	    {},function(answer){
+	      if(answer){
+	      	User.deleteOne(answer)
+	      	.then(function(resp){
+	      		alert('تم حذف المستخدم بنجاح');
+	      		$scope.deleteUser(ev);
+	      	})
+	      	.catch(function(error){
+	      		alert('حدث خطأ اثناء الاضافة, حاول مجددا');
+	      		$scope.deleteUser(ev);
+	      	});
+	      }
+	    },function(){
+	    });
   };
 });
